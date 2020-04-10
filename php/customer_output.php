8c33b86125565fc86189908e083c18f41c8d8926
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-10 16:12:18 +0900

    seg

--- php/customer_output.php
+++ php/customer_output.php
@@ -1,5 +1,5 @@
 <?php
-$pdo=new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=test;charset=utf8', 
+$pdo=new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_e2556b4168bb606;charset=utf8', 
 	'bef1c616153bbe', ':2c68fd8d');
 $sql=$pdo->prepare('insert into customer values(null, ?, ?, ?)');
 if ($sql->execute([$_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password']])) {
