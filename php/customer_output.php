b2186d91a93ca97312c5ce8dca7b1b68e86f5c81
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-10 15:52:57 +0900

    se

--- php/customer_output.php
+++ php/customer_output.php
@@ -1,5 +1,5 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;dbname=test;charset=utf8', 
+$pdo=new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=test;charset=utf8', 
 	'root', '');
 $sql=$pdo->prepare('insert into customer values(null, ?, ?, ?)');
 if ($sql->execute([$_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password']])) {
@@ -12,3 +12,4 @@ if ($sql->execute([$_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password']]
     exit();
 }
 ?>
+
