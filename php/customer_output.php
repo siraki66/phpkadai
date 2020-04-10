5cb0d199fca536a42df5ae7d40eb2b8a3612f38f
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-10 16:59:03 +0900

    seg

--- php/customer_output.php
+++ php/customer_output.php
@@ -1,6 +1,24 @@
 <?php
-$pdo=new PDO('mysql:host=heroku;dbname=test;charset=utf8', 
-	'root', '');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["us-cdbr-iron-east-01.cleardb.net"];
+$username = $url["bef1c616153bbe"];
+$password = $url["2c68fd8d"];
+$db = substr($url["heroku_e2556b4168bb606"], 1);
+
+$pdo = new PDO(
+  'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
+  $username,
+  $password,
+  [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+  ]);
+
+
+
+
 $sql=$pdo->prepare('insert into customer values(null, ?, ?, ?)');
 if ($sql->execute([$_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password']])) {
 	echo '追加に成功しました。';
