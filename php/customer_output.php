bf8658a906c9a1add0ade4890311433568ef78c3
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-10 17:41:35 +0900

    seg

--- php/customer_output.php
+++ php/customer_output.php
@@ -1,20 +1,22 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["us-cdbr-iron-east-01.cleardb.net"];
-$username = $url["bef1c616153bbe"];
-$password = $url["2c68fd8d"];
-$db = substr($url["heroku_e2556b4168bb606"], 1);
 
-$pdo = new PDO(
-  'mysql:host=' . $server . ';dbname=' . $db . ';charset=utf8mb4',
-  $username,
-  $password,
-  [
+
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $db['heroku_e2556b4168bb606'] = ltrim($db[' mysql://bef1c616153bbe:2c68fd8d@us-cdbr-iron-east-01.cleardb.net/heroku_e2556b4168bb606?reconnect=true'], '/');
+  $dsn = "mysql:host={$db['us-cdbr-iron-east-01.cleardb.net']};dbname={$db['heroku_e2556b4168bb606']};charset=utf8";
+  $user = $db['bef1c616153bbe'];
+  $password = $db['2c68fd8d'];
+  $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
-  ]);
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+  );
+  $dbh = new PDO($dsn,$user,$password,$options);
+  return $dbh;
+
+
+
 
 
 
