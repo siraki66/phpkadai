477881e6f1999aa54ac99df18f7d7a979776657a
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-10 16:18:10 +0900

    seg

--- php/customer_output.php
+++ php/customer_output.php
@@ -1,6 +1,6 @@
 <?php
-$pdo=new PDO('mysql:host=us-cdbr-iron-east-01.cleardb.net;dbname=heroku_e2556b4168bb606;charset=utf8', 
-	'bef1c616153bbe', ':2c68fd8d');
+$pdo=new PDO('mysql:host=heroku;dbname=test;charset=utf8', 
+	'root', '');
 $sql=$pdo->prepare('insert into customer values(null, ?, ?, ?)');
 if ($sql->execute([$_REQUEST['name'], $_REQUEST['email'], $_REQUEST['password']])) {
 	echo '追加に成功しました。';
