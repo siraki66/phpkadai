7dc1895f6840b7f527de0c463cd81269e025876f
murasaki6 <nicopan23@yahoo.co.jp>
2020-04-11 20:41:07 +0900

    asfd

--- web/php/index.php
+++ web/php/index.php
@@ -33,6 +33,20 @@ if (isset($_SESSION['customer'])) {
 <tbody>
 
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$db_name = substr($url["path"], 1);
+
+$db_host = $url["host"];
+
+$user = $url["user"];
+
+$password = $url["pass"];
+
+$dsn = "mysql:dbname=".$db_name.";host=".$db_host;
+
+$pdo=new PDO($dsn,$user,$password,array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+
 $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', ''); foreach ($pdo->query('select * from sumaho') as $row) {
      echo '<tr>';
      echo '<p>';
